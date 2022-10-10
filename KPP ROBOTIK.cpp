61f0bf5f8cba4732f4bceec5db0628f22e5e6d91
Wahyuaji1303 <115449970+Wahyuaji1303@users.noreply.github.com>
2022-10-10 13:53:11 +0700

    Add files via upload

---
+++ KPP ROBOTIK.cpp
@@ -0,0 +1,36 @@
+//NAMA      : WAHYU AJI ARTHA FAUZI
+//NRP       : 2039221023
+//JURUSAN   : TEKNIK MESIN INDUSTRI
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+
+tipe_data mencari_V0(tipe_data variabel1, tipe_data variabel2)
+{
+	int V0;
+	int 30;
+	V0 = Vtangensial roller - Losses;
+	V0 = 30 - 5 = 25;
+  	return V0;
+}
+
+tipe_data speed_dgn_loss(tipe_data variabel)
+{
+	int Jarak(integer);
+	Jarak(integer)5 10.0711;
+    return Jarak(integer);
+}
+
+int main() {
+    int 10, 45, 25,
+
+    Xmaks = V02 * sin(2 * SUDUT) / GRAVITASI;
+    Xmaks = 25 * sin(2 * 45) / 10 ;
+    std::cin >> input
+    std::cout << jarak << " " << kecepatan tangensial << std::endl
+    return 0;
+}
